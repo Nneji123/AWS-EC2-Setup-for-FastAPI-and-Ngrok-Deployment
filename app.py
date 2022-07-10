f9161e3a961ea7656553fbcbd0a162548132f715
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2022-07-10 22:24:29 +0100

    modified app and setup files

--- app.py
+++ app.py
@@ -1,60 +1,14 @@
-# USE_NGROK=True uvicorn server:app
-
-# import os
-# import sys
-
 from fastapi import FastAPI
-from fastapi.responses import FileResponse, PlainTextResponse
-# from fastapi.logger import logger
-# from pydantic import BaseSettings
-from colabcode import ColabCode
-cc = ColabCode(port=80, code=False)
+from fastapi.responses import PlainTextResponse
+
 app = FastAPI()
-
-
-# class Settings(BaseSettings):
-#     # ... The rest of our FastAPI settings
-
-#     BASE_URL = "http://localhost:8000"
-#     USE_NGROK = os.environ.get("USE_NGROK", "False") == "True"
-
-
-# settings = Settings()
-
-
-# def init_webhooks(base_url):
-#     # Update inbound traffic via APIs to use the public-facing ngrok URL
-#     pass
-
-
-# # Initialize the FastAPI app for a simple web server
-# app = FastAPI()
-
-# if settings.USE_NGROK:
-#     # pyngrok should only ever be installed or initialized in a dev environment when this flag is set
-#     from pyngrok import ngrok
-
-#     # Get the dev server port (defaults to 8000 for Uvicorn, can be overridden with `--port`
-#     # when starting the server
-#     port = sys.argv[sys.argv.index("--port") + 1] if "--port" in sys.argv else 8000
-
-#     # Open a ngrok tunnel to the dev server
-#     public_url = ngrok.connect(port)
-#     logger.info("ngrok tunnel \"{}\" -> \"http://127.0.0.1:{}/\"".format(public_url, port))
-
-#     # Update any base URLs or webhooks to use the public ngrok URL
-#     settings.BASE_URL = public_url
-#     init_webhooks(public_url)
-
-# # ... Initialize routers and the rest of our app
 
 @app.get("/", response_class=PlainTextResponse, tags=["home"])
 async def home():
     note = """
-Recommendation Engine API 🙌🏻
-This API recommends content such as games, movies, music, books and even anime!
-Note: add "/redoc" to get the complete documentation.
+Simple AWS API 🙌🏻
+Example of how to deploy FastAPI application to an AWS EC2 instance.
   """
     return note
 
-cc.run_app(app=app)
+
--- setup.sh
+++ setup.sh
@@ -1,12 +1,11 @@
 # Update and install requirements
 sudo apt-get update
-sudo apt install python3-pip
+sudo apt install -y python3-pip nginx
 sudo apt install uvicorn
-sudo apt install nginx
 sudo cp -R fastapi_setup /etc/nginx/sites-enabled/
 sudo service nginx restart
 cd ~/AWS-EC2-Setup-for-FastAPI-and-Ngrok-Deployment
 pip3 install -r requirements.txt
 sudo kill -9 $(sudo lsof -t -i:80)
-python3 -m uvicorn app:app --reload --port 8000
+python3 -m uvicorn app:app --reload --host 0.0.0.0
 
