4a4cbd4067a370b4c8235e90d52bab62ffd4ffea
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2022-07-10 22:18:20 +0100

    added configs to nginx server

--- setup.sh
+++ setup.sh
@@ -1,11 +1,12 @@
+# Update and install requirements
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt install python3-pip
+sudo apt install uvicorn
 sudo apt install nginx
-# Official Latest Docker Install
-curl -fsSL https://get.docker.com -o get-docker.sh
-# Run the file
-sudo sh get-docker.sh
-wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
-sudo tar -xvzf ngrok-v3-stable-linux-amd64.tgz
-sudo ngrok authtoken 29vr8YhWZ9CDHrUq2kr0CpUA0e8_6ik4hU5GjJZeAYagTH5i4
-USE_NGROK=True uvicorn app:app --reload --port 80
+sudo cp -R fastapi_setup /etc/nginx/sites-enabled/
+sudo service nginx restart
+cd ~/AWS-EC2-Setup-for-FastAPI-and-Ngrok-Deployment
+pip3 install -r requirements.txt
+sudo kill -9 $(sudo lsof -t -i:80)
+python3 -m uvicorn app:app --reload --port 8000
+
