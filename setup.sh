999607c184a4e42960374ef5f3a409dc7a8139dd
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2022-07-10 22:36:07 +0100

    accepted

--- setup.sh
+++ setup.sh
@@ -1,11 +1,12 @@
 # Update and install requirements
 sudo apt-get update
 sudo apt install -y python3-pip nginx
-sudo apt install uvicorn
+sudo apt install -y uvicorn
 sudo cp -R fastapi_setup /etc/nginx/sites-enabled/
 sudo service nginx restart
 cd ~/AWS-EC2-Setup-for-FastAPI-and-Ngrok-Deployment
 pip3 install -r requirements.txt
 sudo kill -9 $(sudo lsof -t -i:80)
+sudo service nginx restart
 python3 -m uvicorn app:app --reload --host 0.0.0.0
 
