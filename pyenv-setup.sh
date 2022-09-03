5ea40bc06a154a49a52e6592f07543d7bf9502ca
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2022-09-03 18:49:54 +0100

    Update pyenv-setup.sh

--- pyenv-setup.sh
+++ pyenv-setup.sh
@@ -1,27 +1,9 @@
-#!/usr/bin/env bash
-################################################################################
-# pyenv + virtualenv install script for debian/ubuntu linux
-# system requirements: https://github.com/pyenv/pyenv/wiki#suggested-build-environment
-# Install pyenv using,
-#  $ bash pyenv_install.sh
-#
-# Then install your preferred version of python using,
-#  $ pyenv install 3.7.4 -s -v
-#  $ pyenv global 3.7.4
-################################################################################
-
-sudo git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-sudo git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
-echo '
-# pyenv
-export PYENV_ROOT="$HOME/.pyenv"
+#!/bin/sh
+curl https://pyenv.run | bash
+export PATH="$HOME/.pyenv/bin:$PATH"
 export PATH="$PYENV_ROOT/bin:$PATH"
+
 if command -v pyenv 1>/dev/null 2>&1; then
-    sudo eval "$(pyenv init -)"
-    sudo eval "$(pyenv virtualenv-init -)" # Enable auto-activation of virtualenvs
-fi' >> ~/.bashrc
-    source ~/.bashrc
-    exec "$SHELL"
+  eval "$(pyenv init -)"
 fi
-sudo pyenv install 3.8.10
-sudo pyenv global 3.8.10
+pyenv install 3.8.13
